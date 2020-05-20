99f9e24f86c70c1a7b7b575ecf59831bb7140251
Norvon <norvon@126.com>
2020-05-20 13:03:06 +0800

    结构体和类

--- Other/MyPlayground.playground/Pages/类和结构体.xcplaygroundpage/Contents.swift
+++ Other/MyPlayground.playground/Pages/类和结构体.xcplaygroundpage/Contents.swift
@@ -35,3 +35,57 @@ class VideoMode {
     var frameRate = 0.0
     var name: String?
 }
+
+let someResolution = Resolution()
+let someVideoModel = VideoMode()
+
+print("The width of someResolution is \(someResolution.width)")
+
+someVideoModel.resolution.width = 1280
+print("The width of someVideoMod is now \(someVideoModel.resolution.width)")
+
+let vga = Resolution(width: 640, height: 480)
+
+// 结构体和枚举是值类型
+// 值类型是一种当它被指定到常量和变量，或者被传递给函数时会被拷贝的类型
+// 整数，浮点数，布尔量，字符串，数组和字典 都是值类型，并且都是以结构体的形式在后台实现的。
+
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print("cinema is now \(cinema.width) pixels wide")
+print("hd is still \(hd.width) pixels wide")
+
+enum CompassPoint {
+    case North, South, East, West
+}
+var currentDirectiion = CompassPoint.West
+let rememberdDirection = currentDirectiion
+currentDirectiion = .East
+if rememberdDirection == .West {
+    print("The remembered direction is still .West")
+}
+
+// 类是引用类型
+// 不同于值类型，在引用类型被赋值到一个常量，变量或者本身被传递到一个函数的时候它是不会被拷贝的。相对于拷贝，这里使用的是同一个对现存实例的引用
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+
+// 特征运算
+// 应为类是引用类型，在后台有可能有很多常量都是引用到了同一个类的实例。（相同这词对结构体和枚举来说并不是真的相同，因为他们在赋予给常量，变量或者被传递给一个函数时总是被拷贝过去对。）
+// 相同于 ( ===)
+// 不相同于( !==)
+
+if tenEighty === alsoTenEighty {
+    print("tenEighty and alsoTenEighty refer to the same VideoModel instance.")
+}
+
