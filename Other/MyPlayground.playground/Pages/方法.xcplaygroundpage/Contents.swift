292113a16e496abed28c7be1c63f5cf812601310
Norvon <norvon@126.com>
2020-05-29 17:54:00 +0800

    update

--- Other/MyPlayground.playground/Pages/方法.xcplaygroundpage/Contents.swift
+++ Other/MyPlayground.playground/Pages/方法.xcplaygroundpage/Contents.swift
@@ -1,2 +1,42 @@
 import Foundation
 // 实例方法
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+// 在实例方法中修改值类型
+// 结构体和枚举是值类型。默认情况下，值类型属性不能被自身的实例方法修改。
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+//        x += deltaX
+//        y += deltaX
+//    }
+//}
+//
+//var somePoint = Point(x: 1.0, y: 1.0)
+//somePoint.moveBy(x: 2.0, y: 3.0)
+//print("The point is now at (\(somePoint.x), \(somePoint.y))")
+
+struct Point {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveBy(x: 2.0, y: 3.0)
+somePoint.moveBy(x: 3.0, y: 2.0)
+
+// 类型方法
+
