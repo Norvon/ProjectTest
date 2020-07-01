3ed6bb4c24a995e693e2943e77c3b8e0aca1eb6a
Norvon <norvon@126.com>
2020-07-01 23:21:40 +0800

    update

--- Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -20,3 +20,57 @@ func swapTwoValuts<T>(_ a: inout T, _ b: inout T) {
 
 
 // 泛型类型
+struct IntStack {
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+let fromTheTop = stackOfStrings.pop()
+print(fromTheTop)
+
+// 扩展一个泛型类型
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
+
+// 类型约束
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+// 关联类型
+protocol Container {
+    associatedtype ItemType
+    mutating func append(_ item: ItemType)
+    var count: Int { get }
+    subscript(i: Int) -> ItemType { get }
+}
