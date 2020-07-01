cf7055d9870cfb3c3d3f8ac524f794995d35b687
Norvon <norvon@126.com>
2020-07-01 17:59:30 +0800

    update

--- Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -1,2 +1,22 @@
 
 import Foundation
+
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+var someInt = 3
+var another = 107
+swapTwoInts(&someInt, &another)
+print("someInt =  \(someInt)， another = \(another)")
+
+func swapTwoValuts<T>(_ a: inout T, _ b: inout T) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+
+// 泛型类型
