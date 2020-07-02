3d56642d88753d6841f3b8e18d4d012d4157e78e
Norvon <norvon@126.com>
2020-07-02 23:12:31 +0800

    不透明类型

--- Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ Other/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -20,16 +20,16 @@ func swapTwoValuts<T>(_ a: inout T, _ b: inout T) {
 
 
 // 泛型类型
-struct IntStack {
-    var items = [Int]()
-    mutating func push(_ item: Int) {
-        items.append(item)
-    }
-
-    mutating func pop() -> Int {
-        return items.removeLast()
-    }
-}
+//struct IntStack {
+//    var items = [Int]()
+//    mutating func push(_ item: Int) {
+//        items.append(item)
+//    }
+//
+//    mutating func pop() -> Int {
+//        return items.removeLast()
+//    }
+//}
 
 struct Stack<Element> {
     var items = [Element]()
@@ -41,14 +41,14 @@ struct Stack<Element> {
         return items.removeLast()
     }
 }
-var stackOfStrings = Stack<String>()
-stackOfStrings.push("uno")
-stackOfStrings.push("dos")
-stackOfStrings.push("tres")
-stackOfStrings.push("cuatro")
-
-let fromTheTop = stackOfStrings.pop()
-print(fromTheTop)
+//var stackOfStrings = Stack<String>()
+//stackOfStrings.push("uno")
+//stackOfStrings.push("dos")
+//stackOfStrings.push("tres")
+//stackOfStrings.push("cuatro")
+//
+//let fromTheTop = stackOfStrings.pop()
+//print(fromTheTop)
 
 // 扩展一个泛型类型
 extension Stack {
@@ -74,3 +74,64 @@ protocol Container {
     var count: Int { get }
     subscript(i: Int) -> ItemType { get }
 }
+
+struct IntStack: Container {
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+
+    typealias ItemType = Int
+    mutating func append(_ item: Int) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+}
+
+// 给关联类型添加约束
+//protocol SuffixableContainer: Container {
+//    associatedtype Suffix: SuffixableContainer where Suffix.Item == Item
+//    func suffix(_ size: Int) -> Suffix
+//
+//}
+
+extension Array: Container {}
+
+// 泛型Where分句
+
+func allItemsMatch<C1: Container, C2: Container>
+    (_ someContainer: C1, _ anotherContainer: C2) -> Bool where C1.ItemType == C2.ItemType, C1.ItemType: Equatable {
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+
+    return true
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+//if allItemsMatch(stackOfStrings, arrayOfStrings) {
+//    print("All items match.")
+//} else {
+//    print("Not all items match.")
+//}
