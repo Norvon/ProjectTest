d1c67f5afac87099a24c5702410e87d41037aaa3
Norvon <norvon@126.com>
2020-07-08 17:52:30 +0800

    update

--- Other/swift-tips.playground/Pages/多元组 (Tuple).xcplaygroundpage/Contents.swift
+++ Other/swift-tips.playground/Pages/多元组 (Tuple).xcplaygroundpage/Contents.swift
@@ -2,6 +2,20 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//func swapMe1<T>(a: inout T, b: inout T) {
+//    let temp = a
+//    a = b
+//    b = temp
+//}
+//
+//func swapMe2<T>(a: inout T, b: inout T) {
+//    (a,b) = (b, a)
+//}
+
+var a = 3
+var b = 4
+//swapMe1(a: &a, b: &b)
+print(a)
+print(b)
 
 //: [Next](@next)
