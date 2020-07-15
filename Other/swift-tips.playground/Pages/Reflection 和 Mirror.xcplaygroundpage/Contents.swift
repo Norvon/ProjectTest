bb91ac266cfb8b626acf943e4fe8b715ff2e0fe2
Norvon <norvon@126.com>
2020-07-16 00:31:18 +0800

    update

--- Other/swift-tips.playground/Pages/Reflection 和 Mirror.xcplaygroundpage/Contents.swift
+++ Other/swift-tips.playground/Pages/Reflection 和 Mirror.xcplaygroundpage/Contents.swift
@@ -43,5 +43,85 @@ if let name = valueFrom(xiaoMing, key: "books") as? Array<Book> {
 }
 
 // 隐式解包 Optional
+var aNil: String? = nil
 
+var anotherNil: String?? = aNil
+var literalNil: String?? = nil
+
+print(anotherNil)
+if anotherNil == nil {
+    print("anotherNil = nil")
+}
+
+if literalNil == nil {
+    print("literalNil = nil")
+}
+
+let a = [1, 2, 3].map { "\($0 * 2)" }
+print(a)
+
+let num: Int? = 3
+//var result: Int?
+//
+//if let realNum = num {
+//    result = realNum * 2
+//} else {
+//    result = nil
+//}
+
+let result = num.map { $0 * 2}
+
+protocol A2 {
+    func method1() -> String
+}
+
+extension A2 {
+    func method1() -> String {
+        return "hi"
+    }
+
+    func method2() -> String {
+        return "hi"
+    }
+
+}
+
+struct B2: A2 {
+    func method1() -> String {
+        return "hello"
+    }
+
+    func method2() -> String {
+        return "hello"
+    }
+}
+
+let b2 = B2()
+
+b2.method1()
+b2.method2()
+
+let a2 = b2 as A2
+a2.method1() // hello
+a2.method2() // hi
+
+// where 和 模式匹配
+let num2: [Int?] = [48, 99, nil]
+num2.forEach {
+    if let score = $0, score > 60 {
+        print("及格啦 ～\(score)")
+    } else {
+        print(":(")
+    }
+}
+
+extension Sequence where Self.Iterator.Element : Comparable {
+    public func sorted() -> [Self.Iterator.Element]
+}
+
+let sortableArr: [Int] = [3, 1, 2, 4, 5]
+let unsortableArr: [Any?] = ["Hello", 4, nil]
+
+sortableArr.sorted()
+unsortableArr.sorted()
 //: [Next](@next)
