b8031b6db2c57e45cd255e62273f7b2f36f82d69
Norvon <norvon@126.com>
2020-07-16 18:15:16 +0800

    update

--- Other/swift-tips.playground/Pages/Reflection 和 Mirror.xcplaygroundpage/Contents.swift
+++ Other/swift-tips.playground/Pages/Reflection 和 Mirror.xcplaygroundpage/Contents.swift
@@ -115,13 +115,45 @@ num2.forEach {
     }
 }
 
-extension Sequence where Self.Iterator.Element : Comparable {
-    public func sorted() -> [Self.Iterator.Element]
-}
 
 let sortableArr: [Int] = [3, 1, 2, 4, 5]
 let unsortableArr: [Any?] = ["Hello", 4, nil]
 
 sortableArr.sorted()
-unsortableArr.sorted()
+//unsortableArr.sorted()
+
+// indirect 和嵌套 enum
+class Node<T> {
+    let value: T?
+    let next: Node<T>?
+
+    init(value: T?, next: Node<T>?) {
+        self.value = value
+        self.next = next
+    }
+}
+
+let list = Node(value: 1,
+                next: Node(value: 2,
+                           next: Node(value: 3,
+                                      next: Node(value: 4, next: nil))))
+
+indirect enum LinkedList<Element: Comparable> {
+    case empty
+    case node(Element, LinkedList<Element>)
+    
+    func removing(_ element: Element) -> LinkedList<Element> {
+        guard case let .node(value, next) = self else {
+            return .empty
+        }
+        
+        return value == element ? next : LinkedList.node(value, next.removing(element))
+    }
+}
+
+let linkedList = LinkedList.node(1, .node(2, .node(3, .node(4, .empty))))
+
+let result2 = linkedList.removing(2)
+print(result2)
+
 //: [Next](@next)
