86d10122953afac6a82863c826dafa406c478af0
Norvon <norvon@126.com>
2020-07-22 00:04:55 +0800

    update

--- Other/swift-tips.playground/Pages/随机数生成.xcplaygroundpage/Contents.swift
+++ Other/swift-tips.playground/Pages/随机数生成.xcplaygroundpage/Contents.swift
@@ -170,4 +170,67 @@ let image = UIImage(imageName: .myImage)
 
 // Playground 延迟运行
 
+class MyClass1 {
+    @objc func callMe() {
+        print("Hi")
+    }
+}
+let object1 = MyClass1()
+
+//Timer.scheduledTimer(timeInterval: 1,
+//                     target: object1,
+//                     selector: #selector(MyClass1.callMe),
+//                     userInfo: nil,
+//                     repeats: true)
+
+
+let jsonString = """
+{"menu": {
+    "id": "file",
+    "value": "File",
+    "popup": {
+        "menuitem": [
+            {"value": "New", "onclick": "CreateNewDoc()"},
+            {"value": "Open", "onclick": "OpenDoc()"},
+            {"value": "Close", "onclick": "CloseDoc()"}
+        ]
+    }
+}}
+"""
+
+struct Obj: Codable {
+    let menu: Menu
+    struct Menu: Codable {
+        let id: String
+        let value: String
+        let popup: Popup
+    }
+
+    struct Popup: Codable {
+        let menuItem: [MenuItem]
+        enum CodingKeys: String, CodingKey {
+            case menuItem = "menuitem"
+        }
+    }
+
+    struct MenuItem: Codable {
+        let value: String
+        let onClick: String
+
+        enum CodingKeys: String, CodingKey {
+            case value
+            case onClick = "onclick"
+        }
+    }
+}
+
+let data = jsonString.data(using: .utf8)!
+do {
+    let obj = try JSONDecoder().decode(Obj.self, from: data)
+    let value = obj.menu.popup.menuItem[0].value
+    print(value)
+} catch {
+    print("出错了: \(error)")
+}
+
 //: [Next](@next)"
