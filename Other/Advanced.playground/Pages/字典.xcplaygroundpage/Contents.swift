53e42617845bb9eb3abb2b04c280af70f2d80a8b
Norvon <norvon@126.com>
2020-07-24 00:27:46 +0800

    update

--- Other/Advanced.playground/Pages/字典.xcplaygroundpage/Contents.swift
+++ Other/Advanced.playground/Pages/字典.xcplaygroundpage/Contents.swift
@@ -17,5 +17,38 @@ defaultSettings["Name"]
 defaultSettings["name"]
 
 // 可变性
+var userSettings = defaultSettings
+userSettings["Name"] = .text("Jared's iPhone")
+userSettings["Do Not Disturb"] = .bool(true)
+print(userSettings)
+
+let oldName = userSettings.updateValue(.text("Jane's iPhone"), forKey: "Name")
+userSettings["Name"]
+oldName
+
+// 一些有用的字典
+var settings = defaultSettings
+let overriddenSettings: [String: Setting] = ["Name": .text("Overr Jan's iPhone")]
+settings.merge(overriddenSettings, uniquingKeysWith: {$1})
+settings
+
+extension Sequence where Element: Hashable {
+    var frequencies:[Element: Int] {
+        let frequencyPairs = self.map{($0, 1)}
+        return Dictionary(frequencyPairs, uniquingKeysWith: +)
+    }
+}
+
+
+
 
 //: [Next](@next)
+
+
+
+
+
+
+
+
+
