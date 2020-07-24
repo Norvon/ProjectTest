dc3116c87b99685ac48ef814799365cdde6f408b
Norvon <norvon@126.com>
2020-07-25 00:01:54 +0800

    update

--- Other/Advanced.playground/Pages/可选值.xcplaygroundpage/Contents.swift
+++ Other/Advanced.playground/Pages/可选值.xcplaygroundpage/Contents.swift
@@ -3,5 +3,43 @@
 import Foundation
 
 // 通过枚举解决魔法数的问题
+enum Optional<Wrapped> {
+    case none
+    case some(Wrapped)
+}
 
+extension Collection where Element: Equatable {
+    func tFirstIndex(of element: Element) -> Optional<Index> {
+        var idx = startIndex
+        while idx != endIndex {
+            if self[idx] == element {
+                return .some(idx)
+            }
+            formIndex(after: &idx)
+        }
+        return .none
+    }
+}
+
+var array = ["one", "two", "three"]
+let idx = array.tFirstIndex(of: "four")
+// 类型错误
+// array.remove(at: idx)
+switch array.firstIndex(of: "four") {
+case .some(let idx):
+    array.remove(at: idx)
+case .none:
+    break
+}
+
+switch array.firstIndex(of: "one") {
+case let idx?:
+    array.remove(at: idx)
+case nil:
+    print("什么都不做")
+    break
+}
+array
+
+// 可选值概览
 //: [Next](@next)
