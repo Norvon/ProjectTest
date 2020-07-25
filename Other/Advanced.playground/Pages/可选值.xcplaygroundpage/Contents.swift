2918d5cc02be34abf885d52ff2cf49b7b09051a2
Norvon <norvon@126.com>
2020-07-25 17:29:43 +0800

    update

--- Other/Advanced.playground/Pages/可选值.xcplaygroundpage/Contents.swift
+++ Other/Advanced.playground/Pages/可选值.xcplaygroundpage/Contents.swift
@@ -42,4 +42,45 @@ case nil:
 array
 
 // 可选值概览
+array = ["one", "two", "three", "four"]
+if let idx = array.firstIndex(of: "four") {
+    array.remove(at: idx)
+}
+
+array = ["one", "two", "three", "four"]
+if let idx = array.firstIndex(of: "four"), idx != array.startIndex {
+    array.remove(at: idx)
+}
+array
+
+import UIKit
+
+let urlSting = "https://www.objc.io/logo.png"
+if let url = URL(string: urlSting),
+    let data = try? Data(contentsOf: url),
+    let image = UIImage(data: data) {
+    let view = UIImageView(image: image)
+}
+
+// while let 语句和 if let 非常相似，它表示当一个条件返回 nil 时便终止循环。
+let arr = [1, 2, 3]
+var iterator = arr.makeIterator()
+while let i = iterator.next() {
+    print(i, terminator: " ")
+}
+
+print()
+for i in 0..<10 where i % 2 == 0 {
+    print(i, terminator: " ")
+}
+arr
+
+var iterator2 = (0..<10).makeIterator()
+while let i = iterator2.next() {
+    guard i % 2 == 0 else { continue }
+    print(i)
+}
+
+// 双重可选值
+
 //: [Next](@next)
