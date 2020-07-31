f3f04117255c6babc82146bf975a8c47c0285ade
Norvon <norvon@126.com>
2020-07-31 17:56:08 +0800

    update

--- Other/Advanced.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ Other/Advanced.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -2,6 +2,63 @@
 
 import Foundation
 
-var str = "Hello, playground"
+func identity<A>(_ value: A) -> A {
+    return value
+}
+
+enum BinaryTree<Element> {
+    case leaf
+    indirect case node(Element, l: BinaryTree<Element>, r: BinaryTree<Element>)
+    init(_ value: Element) {
+        self = .node(value, l: .leaf, r: .leaf)
+    }
+}
+
+let tree: BinaryTree<Int> = .node(5, l: .leaf, r: .leaf)
+
+// 扩展泛型类型
+extension BinaryTree {
+    var values: [Element] {
+        switch self {
+        case .leaf:
+            return []
+        case let .node(el, left, right):
+            return left.values + [el] + right.values
+        }
+    }
+    
+    func map<T>(_ transform: (Element) -> T) -> BinaryTree<T> {
+        switch self {
+        case .leaf:
+            return .leaf
+        case let .node(el, left, right):
+            return .node(transform(el),
+                         l: left.map(transform),
+                         r: right.map(transform))
+        }
+    }
+}
+
+tree.values
+
+let incremented: BinaryTree<Int> = tree.map { $0 + 1 }
+incremented
+let stringValues: [String] = tree.map { "\($0)" }.values
+
+// 泛型和 Any
+// 基于泛型的设计
+
+struct Resource<A> {
+    let url: URL
+    let parse: (Data) throws -> A
+}
+
+extension URLSession {
+    func load<A>(_ r: Resource<A>,
+                 callback:@escaping (Ressult<A, Error>) -> ()) {
+        dataTask(with: r)
+    }
+}
+
 
 //: [Next](@next)
