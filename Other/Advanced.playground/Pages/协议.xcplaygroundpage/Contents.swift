ab7276e812262bdb6dadbe200fa35c052c63f678
Norvon <norvon@126.com>
2020-08-03 00:49:11 +0800

    update

--- Other/Advanced.playground/Pages/协议.xcplaygroundpage/Contents.swift
+++ Other/Advanced.playground/Pages/协议.xcplaygroundpage/Contents.swift
@@ -42,4 +42,36 @@ extension Equatable {
 }
 
 // 条件化协议实现
+func eqArray<El>(_ eqElement: Eq<El>) -> Eq<[El]> {
+    return Eq { arr1, arr2 in
+        guard arr1.count == arr2.count else { return false }
+        for (l, r) in zip(arr1, arr2) {
+            guard eqElement.eq(l, r) else { return false }
+        }
+        return true
+    }
+}
+
+// 使用协议进行设计
+import UIKit
+protocol DrawingContext {
+    mutating func addEllipse(rect: CGRect, fill: UIColor)
+    mutating func addRectagle(rect: CGRect, fill: UIColor)
+}
+
+extension CGContext: DrawingContext {
+    func addEllipse(rect: CGRect, fill fillColor: UIColor) {
+        setFillColor(fillColor.cgColor)
+        fillEllipse(in: rect)
+    }
+
+    func addRectagle(rect: CGRect, fill fillColor: UIColor) {
+        setFillColor(fillColor.cgColor)
+        fill(rect)
+    }
+}
+
+// 协议组合
+typealias Codable = Decodable & Encodable
+
 //: [Next](@next)
