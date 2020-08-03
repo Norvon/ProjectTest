eb8257addd9a8c7dc3e83899b5f7e238a51f49a7
Norvon <norvon@126.com>
2020-08-04 00:29:13 +0800

    update

---
+++ Other/AppArchitecture/Recordings-MVC/Recordings-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recordings-MVC
+//
+//  Created by Norvon on 2020/8/4.
+//  Copyright © 2020 Norvon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
