7dd9db3f8409996f8e21b896a5cc3fdd73ff5baf
Norvon <norvon@126.com>
2020-08-05 14:30:59 +0800

    update

--- Other/AppArchitecture/Recordings-MVC/Recordings-MVC/App+Views/PlayViewController.swift
+++ Other/AppArchitecture/Recordings-MVC/Recordings-MVC/App+Views/PlayViewController.swift
@@ -20,7 +20,6 @@ class PlayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
