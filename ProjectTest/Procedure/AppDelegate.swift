d931980656ce3bf2d1ab8d55a7cdd9c9f316049f
Norvon <norvon@126.com>
2020-08-08 23:18:34 +0800

    update

--- ProjectTest/Procedure/AppDelegate.swift
+++ ProjectTest/Procedure/AppDelegate.swift
@@ -19,11 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
        return NetworkReachabilityManager(host: "http://app.u17.com")
     }()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    var orientation: UIInterfaceOrientationMask = .portrait
 
-        print("abc".hashValue)
-        print("abc".hashValue)
-        print("abc".hashValue)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         showRootVC()
         return true
@@ -31,3 +29,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+extension UIApplication {
+    class func changeOrientationTo(landscapeRight: Bool) {
+        guard let delegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        if landscapeRight == true {
+            delegate.orientation = .landscapeRight
+            UIApplication.shared.supportedInterfaceOrientations(for: delegate.window)
+            UIDevice.current.setValue(UIInterfaceOrientation.landscapeRight.rawValue, forKey: "orientation")
+        } else {
+            delegate.orientation = .portrait
+            UIApplication.shared.supportedInterfaceOrientations(for: delegate.window)
+            UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+        }
+    }
+}
