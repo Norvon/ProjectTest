e39407e30bf43b02ebd0d7127bed95b3791feebc
Norvon <norvon@126.com>
2020-08-10 18:02:42 +0800

    update

---
+++ ProjectTest/Procedure/Home/VC/UBoutiqueListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UBoutiqueListViewController.swift
+//  ProjectTest
+//
+//  Created by nor on 2020/8/10.
+//  Copyright © 2020 nor. All rights reserved.
+//
+
+import UIKit
+
+class UBoutiqueListViewController: UBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
